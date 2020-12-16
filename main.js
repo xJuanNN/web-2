8429fd05d196cb1cf7b1f7a89d4153c623e10d0c
xJuanNN <68256758+xJuanNN@users.noreply.github.com>
2020-12-15 21:07:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
+
